a59b366ca1415d08cf6e1ad7cf571ab3eb142105
alswjdghks <jerrymin1221@naver.com>
2025-05-25 22:55:22 +0900

    feat: BaseTimeEntity 설정

---
+++ src/main/java/com/elasticsearch/practice/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.elasticsearch.practice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaAuditingConfig {
+}
---
+++ src/main/java/com/elasticsearch/practice/domain/BaseTimeEntity.java
@@ -0,0 +1,26 @@
+package com.elasticsearch.practice.domain;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+public class BaseTimeEntity {
+
+    @Column(updatable = false)
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
+
+}
