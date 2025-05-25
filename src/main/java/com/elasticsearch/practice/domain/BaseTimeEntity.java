89433fbd4788f6092f2914a2b8d0f2fd29fe55d8
alswjdghks <jerrymin1221@naver.com>
2025-05-25 23:31:59 +0900

    feat: ElasticSearch 자동완성1을 위한 n-gram 분석기 설정 및 매핑구성 설정 수정

--- src/main/java/com/elasticsearch/practice/domain/BaseTimeEntity.java
+++ src/main/java/com/elasticsearch/practice/domain/BaseTimeEntity.java
@@ -7,6 +7,8 @@ import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
@@ -18,9 +20,11 @@ public class BaseTimeEntity {
 
     @Column(updatable = false)
     @CreatedDate
+    @Field(type = FieldType.Date, format = {}, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS||epoch_millis")
     private LocalDateTime createdDate;
 
     @LastModifiedDate
+    @Field(type = FieldType.Date, format = {}, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS||epoch_millis")
     private LocalDateTime lastModifiedDate;
 
 }
